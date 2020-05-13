88b8296bf19ee64e77262ce4ab7202a1f92034f6
liuyu <liuyuit@aliyun.com>
2020-05-13 17:42:20 +0800

    新增包

---
+++ src/Trois/Library/Loader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/5/13
+ * Time: 17:36
+ */
+
+namespace Trois\Library;
+
+class Loader{
+    public function index(){
+        echo 'Hello Loader';
+    }
+}
